336e99e7bb922e23cf20bd2620930c427ded6989
vicSystems <victorasuquob@gmail.com>
2023-05-16 13:56:11 +0100

    version upgrade

---
+++ app/Http/Controllers/API/V1/InvoiceController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+use App\Models\Invoice;
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Config;
+use App\Http\Requests\StoreInvoiceRequest;
+use App\Http\Requests\UpdateInvoiceRequest;
+
+use Illuminate\Http\Request;
+
+
+class InvoiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreInvoiceRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreInvoiceRequest $request)
+    {
+        //
+
+        Invoice::updateOrCreate(
+            [
+                'user_id' => $request->user()->id,
+                'invoice_code' => $request->invoice_code
+            ],
+            [
+            'user_id' => $request->user()->id,
+            'invoice_code' => $request->invoice_code,
+            'total_amount' => 0
+
+        ]);
+
+        return Invoice::where('invoice_code', $request->invoice_code)->first();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Invoice  $invoice
+     * @return \Illuminate\Http\Response
+     */
+    public function show($invoice_code, Request $request)
+    {
+        //
+        
+
+        Config::set('database.connections.mysql.database', $request->storeDb);
+
+
+        $invoiceData = Invoice::with('invoice_items.products')->where('invoice_code', $invoice_code)->first();
+
+        return $invoiceData;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateInvoiceRequest  $request
+     * @param  \App\Models\Invoice  $invoice
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateInvoiceRequest $request, Invoice $invoice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Invoice  $invoice
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Invoice $invoice)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\DashboardController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [PageController::class,'index'])->middleware('checkdb');
+
+Route::get('/about', [PageController::class,'about']);
+
+Route::get('/contact', [PageController::class,'contact']);
+
+Route::get('/cart', [PageController::class,'cart']);
+
+Route::get('/auth', [PageController::class,'auth'])->middleware('checkdb');
+
+Route::get('/checkout', [PageController::class,'checkout']);
+
+Route::get('/payment-successful', function () {
+    return view('payment_successful');
+});
+
+
+Route::get('/admin', [DashboardController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
